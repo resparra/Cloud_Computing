c77074ae32d910594258f03f7f7b3b3e7bacb242
resparra <rafael.esparra1@upr.edu>
2013-04-27 01:32:45 -0300

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,17 @@
+// Code goes here
+function Ctrl($scope) {
+           $scope.submit = function() {
+        if (this.text) {
+           if (this.text == "I love computers"){
+             $("#video").html('<video width="320" height="240" controls autoplay><source src="https://dl.dropboxusercontent.com/u/10068994/i_love_comp.m4v" type="video/mp4"></video>');
+           }
+        if (this.text == "I broke the teapot"){
+             $("#video").html('<video width="320" height="240" controls autoplay><source src="https://dl.dropboxusercontent.com/u/10068994/i_break_teapot.m4v" type="video/mp4"></video>');
+           }
+        if (this.text == "Today we can help the deaf community"){
+             $("#video").html('<video width="320" height="240" controls autoplay><source src="https://dl.dropboxusercontent.com/u/10068994/can_help.m4v" type="video/mp4"></video>');
+           }
+           this.text = '';
+           }
+        };
+      }
